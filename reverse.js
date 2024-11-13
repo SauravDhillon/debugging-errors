ad7498da7e1b3ce3ab0ed4481416f5112d36c6d6
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-13 12:55:41 -0500

    Changes were made to reverse.js file

--- reverse.js
+++ reverse.js
@@ -1,8 +1,9 @@
 var input = process.argv[2];
 
-if (input)
+if (input){
   console.log(reverse(input));
-
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
 }
+
+function reverse() {
+  return input.split('').reverse().join('');
+};
